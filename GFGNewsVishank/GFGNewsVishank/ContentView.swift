c48697f99c2873c5972e726a5c4e27ef60aaccf3
Vishank Raghav <vishank13@gmail.com>
2022-02-24 17:40:23 +0530

    Project Setup

---
+++ GFGNewsVishank/GFGNewsVishank/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GFGNewsVishank
+//
+//  Created by Bhanu Kaushik on 24/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GFGNewsVishank/GFGNewsVishank/GFGNewsVishankApp.swift
@@ -0,0 +1,17 @@
+//
+//  GFGNewsVishankApp.swift
+//  GFGNewsVishank
+//
+//  Created by Bhanu Kaushik on 24/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GFGNewsVishankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
